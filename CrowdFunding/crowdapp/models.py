75205d95989d071990bc7e59dfd9e6b7712a4e42
Mark-andrew7 <kamaumark19@gmail.com>
2024-08-30 16:58:45 +0300

    Declared models

--- CrowdFunding/crowdapp/models.py
+++ CrowdFunding/crowdapp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class userProfile(models.Model):
+  USER_TYPES = (
+    ('investor', 'Investor'),
+    ('advisor', 'Advisor'),
+    ('business', 'Business')
+  )
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  user_type = models.CharField(max_length=100, choices=USER_TYPES)
+  name = models.CharField(max_length=100)
+  description = models.CharField(max_length=100)
+  amount = models.IntegerField()
+  investment_type = models.CharField(max_length=100)
+
+
+class investment(models.Model):
+  name = models.CharField(max_length=100)
+  investor = models.ForeignKey('User', on_delete=models.CASCADE)
+  description = models.CharField(max_length=100)
+  value_per_share = models.FloatField()
+  is_approved = models.BooleanField(default=False)
+  investment_type = models.CharField()
+  valuation = models.FloatField()
+  amount = models.IntegerField()
+
+
+class shares(models.Model):
+  investor = models.ForeignKey('User', on_delete=models.CASCADE)
+  advisor = models.ForeignKey('User', on_delete=models.CASCADE)
+  investment = models.ForeignKey('investment', on_delete=models.CASCADE)
+  value = models.IntegerField()
+  
--- CrowdFunding/crowdapp/views.py
+++ CrowdFunding/crowdapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def signup(request):
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+    email = request.POST['email']
